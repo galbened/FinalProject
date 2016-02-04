6127d50f2cf542c7b2eab95ac4d3dd36ecbc0866
galbened <galbened@post.bgu.ac.il>
2016-02-04 02:10:10 +0200

    loginActivity

---
+++ MyAppMo/app/src/main/java/com/example/moriya/myappmo/LoginActivity.java
@@ -0,0 +1,50 @@
+package com.example.moriya.myappmo;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button bLogin;
+    EditText etUsername;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        bLogin = (Button) findViewById(R.id.bLogin);
+        etUsername = (EditText) findViewById(R.id.etUsername);
+
+        bLogin.setOnClickListener(this);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId())
+        {
+            case R.id.bLogin:
+                startActivity(new Intent(this, MainActivityMo.class));
+                break;
+        }
+    }
+}
