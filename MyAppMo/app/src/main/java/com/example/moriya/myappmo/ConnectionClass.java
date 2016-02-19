910112ddb346c4300697d386d2b00b9c2d1bb6ce
galbened <galbened@post.bgu.ac.il>
2016-02-19 17:53:49 +0200

    DB

---
+++ MyAppMo/app/src/main/java/com/example/moriya/myappmo/ConnectionClass.java
@@ -0,0 +1,44 @@
+package com.example.moriya.myappmo;
+
+import android.annotation.SuppressLint;
+import android.os.StrictMode;
+import android.util.Log;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ * Created by galVM on 2/19/2016.
+ */
+public class ConnectionClass {
+    String ip = "pinpadproject.cmznqw6b5abs.us-west-2.rds.amazonaws.com:1433"; //1433
+    String classs = "net.sourceforge.jtds.jdbc.Driver";
+    String db = "pinpadDB";
+    String username = "pinpad";
+    String password = "catsrfun";
+
+    @SuppressLint("NewApi")
+    public Connection CONN() {
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder()
+                .permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+        Connection conn = null;
+        String ConnURL = null;
+        try {
+            Class.forName(classs);
+            ConnURL = "jdbc:jtds:sqlserver://" + ip + ";"
+                    + "databaseName=" + db + ";user=" + username + ";password="
+                    + password + ";";
+            conn = DriverManager.getConnection(ConnURL);
+        } catch (SQLException se) {
+            Log.e("ERRO", se.getMessage());
+        } catch (ClassNotFoundException e) {
+            Log.e("ERRO", e.getMessage());
+        } catch (Exception e) {
+            Log.e("ERRO", e.getMessage());
+        }
+        return conn;
+    }
+}
+
--- MyAppMo/app/src/main/java/com/example/moriya/myappmo/LoginActivity.java
+++ MyAppMo/app/src/main/java/com/example/moriya/myappmo/LoginActivity.java
@@ -9,11 +9,18 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
 
     Button bLogin;
     EditText etUsername;
+    ConnectionClass connectionClass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +29,7 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
 
         bLogin = (Button) findViewById(R.id.bLogin);
         etUsername = (EditText) findViewById(R.id.etUsername);
+        connectionClass = new ConnectionClass();
 
         bLogin.setOnClickListener(this);
 
@@ -43,7 +51,32 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         switch (v.getId())
         {
             case R.id.bLogin:
-                startActivity(new Intent(this, MainActivityMo.class));
+                Connection con = connectionClass.CONN();
+
+                String query = "select * from Users where userName='" + etUsername.getText() + "'";
+                Statement stmt = null;
+                try {
+                    stmt = con.createStatement();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+                String fullName = "";
+                try {
+                    ResultSet rs = stmt.executeQuery(query);
+                    if(!rs.next()) {
+                        Toast.makeText(getApplicationContext(), "Username " + etUsername.getText() + " does not exist.",
+                                Toast.LENGTH_LONG).show();
+                        break;
+                    }
+                    fullName = rs.getString("firstName") + " " + rs.getString("secondName");
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+                Intent intent = new Intent(this, MainActivityMo.class);
+                Bundle b = new Bundle();
+                b.putString("key",fullName);
+                intent.putExtras(b);
+                startActivity(intent);
                 break;
         }
     }
